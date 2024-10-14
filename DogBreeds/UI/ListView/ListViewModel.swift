47daf13b563b27f59df5c9364d0fabe3245eaf1b
Ahmed Alnaqah <alnaqah@huez.se>
2024-10-14 03:41:24 +0200

    Add list view model

--- DogBreeds/UI/ListView/ListViewModel.swift
+++ DogBreeds/UI/ListView/ListViewModel.swift
@@ -1,1 +1,21 @@
+import Foundation
 
+@MainActor
+final class ListViewModel: ObservableObject, Sendable {
+    @Published var breeds: [DogBreed]
+    private let navigationDelegate: ListNavigationDelegate
+    
+    init(
+        initialBreeds: [DogBreed] = [],
+        navigationDelegate: ListNavigationDelegate
+    ) {
+        self.breeds = initialBreeds
+        self.navigationDelegate = navigationDelegate
+    }
+    
+    func fetchBreeds() async {}
+    
+    func openDetails(for breed: DogBreed) {
+        navigationDelegate.openDetails(id: breed.id)
+    }
+}
