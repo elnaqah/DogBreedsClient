7c97d243e429ec4734a6293bc57bfb2f871d26b6
Ahmed Alnaqah <alnaqah@huez.se>
2024-10-14 03:42:00 +0200

    Add list view implementation

--- DogBreeds/UI/ListView/ListView.swift
+++ DogBreeds/UI/ListView/ListView.swift
@@ -1,8 +1,30 @@
 import SwiftUI
 
 struct ListView: View {
+    @ObservedObject var viewModel: ListViewModel
     
     var body: some View {
-        Text("List")
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                ForEach(viewModel.breeds, id: \.id) { breed in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(breed.name)
+                                .font(.headline)
+                            Text("Size: \(breed.size)")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.vertical, 8)
+                    .onTapGesture {
+                        viewModel.openDetails(for: breed)
+                    }
+                }
+            }
+        }
+        .task {
+            await viewModel.fetchBreeds()
+        }
     }
 }
